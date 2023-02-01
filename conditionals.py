584788fb978e8127aeaa1c668b03dcf6d3ac7560
rafacovez <adanescollante@gmail.com>
2023-01-31 21:01:56 -0400

    coded is_criticality_balanced function

--- conditionals.py
+++ conditionals.py
@@ -14,7 +14,7 @@ def is_criticality_balanced(temperature, neutrons_emitted):
     - The product of temperature and neutrons emitted per second is less than 500000.
     """
 
-    pass
+    return (temperature < 800 and neutrons_emitted > 500 and temperature * neutrons_emitted < 500000)
 
 
 def reactor_efficiency(voltage, current, theoretical_max_power):
